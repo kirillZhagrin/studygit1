107aaf98eaebd640444a91d7ba1eec01e336f038
Kirill <example@gmail.com>
2023-04-09 14:25:15 +0200

    add css file

---
+++ scriptuha.js
@@ -0,0 +1,32 @@
+for (let i = 0;  i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+let result ='';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+
+    for(let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+first: for (let i = 0;  i < 3; i++) {
+    console.log(`First level: ${i}`);
+    second: for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        third: for(let k = 0; k < 5; k++) {
+            if (k === 2) break first;
+        console.log(`Third level: ${k}`);
+        }
+    } 
+}
+
